da923843461467dc1bf91835626ada8dae99447d
ThanhNgo1007 <nhthanh1007@gmail.com>
2025-10-29 10:19:24 +0700

    Initial commit - upload Spring Boot project

--- src/main/java/com/furniture/controller/AuthController.java
+++ src/main/java/com/furniture/controller/AuthController.java
@@ -1,9 +1,33 @@
 package com.furniture.controller;
 
+import com.furniture.modal.User;
+import com.furniture.repository.UserRepository;
+import com.furniture.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req) {
+
+        User user = new User();
+        user.setEmail(req.getEmail());
+        user.setFullName(req.getFullName());
+
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
 
 
 }
